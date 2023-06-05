ec901bd53340be9f7a0c9b0605f50809eb7f816c
meet_hansalia <meethansalia99@gmail.com>
2023-06-05 16:16:52 +0530

    second commit

---
+++ learning-react/src/component/welcome.js
@@ -0,0 +1,24 @@
+import { useEffect,useState } from "react";
+  
+function Tick(props){
+    const[Time, setTime]= useState(new Date())
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setTime(new Date())
+        }, 800);
+      
+        return () => clearInterval(interval);
+      }, []);
+
+    return<div>
+            <h1>Hello World</h1>
+            <h1>I am Meet Hansalia</h1>
+            <h1>it is :{Time.toLocaleTimeString()}</h1>
+            <h1>heello</h1>
+        </div>
+    
+}
+
+
+
+export default Tick;
